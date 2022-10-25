d85085a8417e953502dcae967a68249a8b3dd037
Viktoria Domashchuk <domashchukviktoria@gmail.com>
2022-10-25 17:06:52 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,9 +3,13 @@ const input = document.querySelector('#validation-input');
 input.addEventListener('blur', onValidInput);
 
 function onValidInput() {
-  if (input.value.length === Number(input.dataset.length)) {
+  if (input.value.length == input.dataset.length) {
+    input.classList.remove('invalid');
     input.classList.add('valid');
   } else {
+    input.classList.remove('valid');
     input.classList.add('invalid');
   }
 }
+
+
